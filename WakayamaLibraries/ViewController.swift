9c5a89231bded4a316a97211436bfd3d11115597
Miura <ika020202@gmail.com>
2023-02-12 14:50:39 +0900

    first

---
+++ WakayamaLibraries/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WakayamaLibraries
+//
+//  Created by KairiMiura on 2023/02/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
